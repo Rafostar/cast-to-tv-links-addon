b2bd4cd82644225531e686a85f9a0bc90e538069
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-09-04 09:35:25 +0200

    Use PWD to get extensions path and imports cleanup

--- extension.js
+++ extension.js
@@ -4,19 +4,19 @@ Developer: Rafostar
 Extension GitHub: https://github.com/Rafostar/cast-to-tv-links-addon
 */
 const Local = imports.misc.extensionUtils.getCurrentExtension();
-const extensionsPath = Local.path.substring(0, Local.path.lastIndexOf('/'));
-const mainPath = extensionsPath + '/cast-to-tv@rafostar.github.com';
+
+const EXTENSIONS_PATH = Local.path.substring(0, Local.path.lastIndexOf('/'));
+const MAIN_PATH = EXTENSIONS_PATH + '/cast-to-tv@rafostar.github.com';
+const GETTEXT_DOMAIN = Local.metadata['gettext-domain'];
+const EXTENSION_ID = Local.metadata['extension-id'];
+const DELAY = 1500;
 
 /* Imports from main extension */
-imports.searchPath.unshift(mainPath);
+imports.searchPath.unshift(MAIN_PATH);
 const Widget = Local.imports.widget;
 const Addons = imports.addons;
 const Helper = imports.helper;
 imports.searchPath.shift();
-
-const GETTEXT_DOMAIN = Local.metadata['gettext-domain'];
-const EXTENSION_ID = Local.metadata['extension-id'];
-const DELAY = 1500;
 
 function init()
 {
