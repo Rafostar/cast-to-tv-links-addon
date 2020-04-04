960055a3643175e28adfcc7006fac9bc3e2cde56
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-04-04 13:02:44 +0200

    Port prefs to GObject

--- links_prefs.js
+++ links_prefs.js
@@ -1,6 +1,6 @@
 imports.gi.versions.Gtk = '3.0';
 
-const { Gio, Gtk } = imports.gi;
+const { Gio, Gtk, GObject } = imports.gi;
 const Local = imports.misc.extensionUtils.getCurrentExtension();
 
 const EXTENSIONS_PATH = Local.path.substring(0, Local.path.lastIndexOf('/'));
@@ -9,7 +9,7 @@ const MAIN_PATH = EXTENSIONS_PATH + '/cast-to-tv@rafostar.github.com';
 
 /* Imports from main extension */
 imports.searchPath.unshift(MAIN_PATH);
-const { SettingLabel } = imports.prefs_shared;
+const { SettingLabel, addToGrid } = imports.prefs_shared;
 const Helper = imports.helper;
 imports.searchPath.shift();
 
@@ -23,18 +23,19 @@ function init()
 	Helper.initTranslations(LOCAL_PATH, Metadata['gettext-domain']);
 }
 
+let LinksSettings = GObject.registerClass(
 class LinksSettings extends Gtk.Grid
 {
-	constructor()
+	_init()
 	{
-		super({ margin: 20, row_spacing: 6 });
+		super._init({ margin: 20, row_spacing: 6 });
 		this.title = new Gtk.Label({ label: _("Links") });
 		let label = null;
 		let widget = null;
 
 		/* Label: Links Options */
 		label = new SettingLabel(_("Links Options"), true);
-		this.attach(label, 0, 0, 1, 1);
+		addToGrid(this, label, null, true);
 
 		/* Preferred youtube-dl Format */
 		label = new SettingLabel(_("Preferred format"));
@@ -42,16 +43,14 @@ class LinksSettings extends Gtk.Grid
 		widget.append('combined', _("Best seekable"));
 		widget.append('separate', _("Best quality") + ' ' + _("(experimental)"));
 		Settings.bind('ytdl-mode', widget, 'active-id', Gio.SettingsBindFlags.DEFAULT);
-		this.attach(label, 0, 1, 1, 1);
-		this.attach(widget, 1, 1, 1, 1);
+		addToGrid(this, label, widget);
 
 		/* youtube-dl Path */
 		label = new SettingLabel(_("Path to youtube-dl"));
 		widget = new Gtk.Entry({ width_request: 220, halign:Gtk.Align.END });
 		widget.set_placeholder_text("/usr/bin/youtube-dl");
 		Settings.bind('ytdl-path', widget, 'text', Gio.SettingsBindFlags.DEFAULT);
-		this.attach(label, 0, 2, 1, 1);
-		this.attach(widget, 1, 2, 1, 1);
+		addToGrid(this, label, widget);
 
 		/* Max Video Quality */
 		/* TRANSLATORS: Can be translated simply as "Max quality" to make it shorter */
@@ -69,38 +68,34 @@ class LinksSettings extends Gtk.Grid
 		widget.append('60', '60 fps');
 		Settings.bind('max-fps', widget, 'active-id', Gio.SettingsBindFlags.DEFAULT);
 		hbox.pack_start(widget, false, false, 0);
-		this.attach(label, 0, 3, 1, 1);
-		this.attach(hbox, 1, 3, 1, 1);
+		addToGrid(this, label, hbox);
 
 		/* Allow VP9 */
 		label = new SettingLabel(_("Allow VP9 codec"));
 		widget = new Gtk.Switch({ halign:Gtk.Align.END });
 		widget.set_active(Settings.get_boolean('allow-vp9'));
 		Settings.bind('allow-vp9', widget, 'active', Gio.SettingsBindFlags.DEFAULT);
-		this.attach(label, 0, 4, 1, 1);
-		this.attach(widget, 1, 4, 1, 1);
+		addToGrid(this, label, widget);
 
 		/* Label: Subtitles */
 		label = new SettingLabel(_("Subtitles"), true, true);
-		this.attach(label, 0, 5, 1, 1);
+		addToGrid(this, label);
 
 		/* Preferred Lang */
 		label = new SettingLabel(_("Preferred language"));
 		widget = new Gtk.Entry({ halign:Gtk.Align.END });
 		widget.set_placeholder_text("en");
 		Settings.bind('preferred-lang', widget, 'text', Gio.SettingsBindFlags.DEFAULT);
-		this.attach(label, 0, 6, 1, 1);
-		this.attach(widget, 1, 6, 1, 1);
+		addToGrid(this, label, widget);
 
 		/* Fallback Lang */
 		label = new SettingLabel(_("Fallback language"));
 		widget = new Gtk.Entry({ halign:Gtk.Align.END });
 		widget.set_placeholder_text(_("none"));
 		Settings.bind('fallback-lang', widget, 'text', Gio.SettingsBindFlags.DEFAULT);
-		this.attach(label, 0, 7, 1, 1);
-		this.attach(widget, 1, 7, 1, 1);
+		addToGrid(this, label, widget);
 	}
-}
+});
 
 function buildPrefsWidget()
 {
